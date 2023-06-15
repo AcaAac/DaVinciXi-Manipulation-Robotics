71154a6b0ca6dbae9c038d06ee018db9836e7e89
AcaAac <afonso.d.araujo@tecnico.ulisboa.pt>
2023-06-15 17:27:35 +0100

    Trajectory

---
+++ LAB2_DYNAMICS/Testing_tauNewtonEuler.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = Testing_tauNewtonEuler(inputArg1,inputArg2)
+%TESTING_TAUNEWTONEULER Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
